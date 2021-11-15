33e26418aa53f036638376e28fe8287c5e68b6bc
Raul <raulfernan000@gmail.com>
2021-11-15 15:25:24 +0100

    add-player.js updated

--- scripts/modules/add-player.js
+++ scripts/modules/add-player.js
@@ -1,11 +1,40 @@
-export function addPlayer() {
-    var playerName = document.getElementById('player-input').value
-    var listCheck = localStorage.getItem('playerList')
-    if (listCheck == null) {
-        localStorage.setItem('playerList', playerName)
-    }
-    if (listCheck != null) {
-        localStorage.setItem('playerList', localStorage.getItem('playerList') + " # " + playerName)
-    }
-    console.log('test')
+export function inyectPlayer(name) {
+    let list = document.getElementById('player-list')
+    var newItem = `
+    <p> ${name} </p>
+    `
+    list.innerHTML += newItem
+    console.log(typeof(playerList));
 }
+
+var playerList = []
+
+export function addPlayer() {
+    let playerName = document.getElementById('player-input').value
+    playerList += [playerName]
+    inyectPlayer(playerName)
+    return playerList
+}
+
+// export function addPlayer() {
+//     let playerName = document.getElementById('player-input').value
+//     if ( playerList == null) {
+//         const playerList = []
+//         return playerList
+//     }
+//     playerList += playerName
+//     inyectPlayer(playerName)
+//     return playerList
+// }
+
+// export function addPlayer() {
+//     let playerName = document.getElementById('player-input').value
+//     let listCheck = localStorage.getItem('playerList')
+//     if (listCheck == null) {
+//         localStorage.setItem('playerList', playerName)
+//     }
+//     if (listCheck != null) {
+//         localStorage.setItem('playerList', listCheck + "#" + playerName)
+//     }
+//     inyectPlayer(playerName)
+// }
