1a01d25fcb6ab0740d7cde155c357c29cb8e12a1
Raul <raulfernan000@gmail.com>
2021-11-15 14:29:46 +0100

    addplayer.js updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,8 @@
 import { addPlayer } from "./modules/add-player.js";
 import { carrouselAnimation } from "./modules/animations.js";
+
+const addBtn = document.getElementById('btn-add-player')
+
+addBtn.onclick = addPlayer
+
+window.onload = localStorage.clear()
--- scripts/modules/add-player.js
+++ scripts/modules/add-player.js
@@ -1,10 +1,11 @@
-export function addPlayer()
-{
+export function addPlayer() {
     var playerName = document.getElementById('player-input').value
     var listCheck = localStorage.getItem('playerList')
-    if ( listCheck == null) {
-        localStorage.setItem("playerList", [])
+    if (listCheck == null) {
+        localStorage.setItem('playerList', playerName)
     }
-    localStorage.setItem("playerList", playerName)
+    if (listCheck != null) {
+        localStorage.setItem('playerList', localStorage.getItem('playerList') + " # " + playerName)
+    }
     console.log('test')
 }
